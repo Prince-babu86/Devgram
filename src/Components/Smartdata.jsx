b4ccbc2ffdb5918ff73e00e65396987cab690571
Prince Babu <princebabu4495@gmail.com>
2025-03-27 02:19:42 +0530

    new change

--- src/Components/Smartdata.jsx
+++ src/Components/Smartdata.jsx
@@ -6,7 +6,7 @@ const Smartdata = () => {
         <div className="conatiner flex w-full gap-3 overflow-y-auto">
             <div className="smt bg-[#A88DFE] h-[120px] w-[140px] rounded-xl relative">
                 <h1 className='text-white p-2 text-xl'>Password</h1>
-                <i className="ri-lock-unlock-fill text-[#020202] opacity-80  text-5xl absolute right-3 bottom-3"></i>
+                <i className="ri-lock-password-fill text-[#020202] opacity-80  text-5xl absolute right-3 bottom-3"></i>
             </div>
 
             <div className="smt bg-[#FDA94D] h-[120px] w-[140px] rounded-xl relative">
