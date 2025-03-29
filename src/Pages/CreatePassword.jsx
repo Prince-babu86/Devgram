2ed626eb3028ca74c860e93a5c962d3355bef1c5
Prince Babu <princebabu4495@gmail.com>
2025-03-30 02:28:49 +0530

    new changes

--- src/Pages/CreatePassword.jsx
+++ src/Pages/CreatePassword.jsx
@@ -3,67 +3,121 @@ import { useNavigate } from "react-router-dom";
 import Signup from "./Signup";
 
 const CreatePassword = () => {
-  const [userPassword, setuserPassword] = useState([{
-    website:"",
-    username:"",
-    password:"",
-    // imageId:""
-  }])
+  const [userPassword, setuserPassword] = useState([
+    {
+      website: "",
+      username: "",
+      password: "",
+      // imageId:""
+    },
+  ]);
 
-  const [loader, setloader] = useState(false)
-  const navigate = useNavigate()
-  const imageRef = useRef()
-  const userdata = JSON.parse(localStorage.getItem("userdata"))
-  const handleOnChange  = (e) =>{
-    setuserPassword({...userPassword , [e.target.name]:e.target.value})
-  }
+  const [loader, setloader] = useState(false);
+  const navigate = useNavigate();
+  const imageRef = useRef();
+  const userdata = JSON.parse(localStorage.getItem("userdata"));
+  const handleOnChange = (e) => {
+    setuserPassword({ ...userPassword, [e.target.name]: e.target.value });
+  };
 
-
-  const handleOnSubmit = (e) =>{
-    e.preventDefault()
-    const storedPassword = JSON.parse(localStorage.getItem("userPasswords")) || []
-    storedPassword.push({...userPassword})
-    localStorage.setItem("userPasswords" , JSON.stringify(storedPassword))
-    setloader(true)
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const storedPassword =
+      JSON.parse(localStorage.getItem("userPasswords")) || [];
+    storedPassword.push({ ...userPassword });
+    localStorage.setItem("userPasswords", JSON.stringify(storedPassword));
+    setloader(true);
     setTimeout(() => {
-      setloader(false)
-      navigate("/")
+      setloader(false);
+      navigate("/");
     }, 2000);
-  }
+  };
 
   return (
-   <>
-  
-   {userdata ? ( <div className="h-screen w-full bg-[#FFCB47] overflow-hidden">
-    <div className="h-[20vh] w-full  px-4 py-4 "> 
-      <div className="flex items-center gap-5">
-      <i onClick={()=>{navigate("/")}} className="ri-arrow-left-line text-2xl active:scale-[1.5]"></i>
-      <h4>Back</h4>
-      </div>
-      <h1 className="text-3xl font-semibold text-center mt-5">Store Your Password</h1>
-      <p className="text-sm font-semibold text-center">Welcome to Password manager to store your password</p>
-    </div>
-      <div className="crtprt-center relative px-4 py-6 flex w-full items-start mt-24 h-[77vh] rounded-t-4xl  bg-white">
-       
-        <form onSubmit={handleOnSubmit} action="" className="flex items-start flex-col w-full mt-12">
-         <h4 className="absolute top-6 left-[35%] w-full text-xl font-semibold ">Get Started</h4>
-          <input ref={imageRef} required minLength={4} maxLength={16} onChange={handleOnChange} value={userPassword.website} name="website" className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm" placeholder="Enter Your Website ? App Name" type="text" />
-          <input required minLength={4} maxLength={16} onChange={handleOnChange} value={userPassword.username} name="username" className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm" placeholder="Enter Your Username, Email, userid" type="text" />
-          <input required minLength={4} maxLength={16} onChange={handleOnChange} value={userPassword.password} name="password" className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm" placeholder="Enter Your Password" type="password" />
-          <button type="submit" className="text-lg bg-[#FFCB47] w-full py-3 rounded-xl mt-2">{loader ? (<div className="flex justify-center gap-2 items-center">
-            <div className="h-6 w-6 animate-spin inline-block border-t-transparent rounded-full border-gray-700 border-[3px]"></div>Loading
-          </div>):("Create")}
-           
-          </button>
-          <>
-          
-          </>
-        </form>
-
-       
-      </div>
-    </div>):(<Signup/>)}
-   </>
+    <>
+      {userdata ? (
+        <div className="h-screen w-full bg-[#FFCB47] overflow-hidden">
+          <div className="h-[20vh] w-full  px-4 py-4 ">
+            <div className="flex items-center gap-5">
+              <i
+                onClick={() => {
+                  navigate("/");
+                }}
+                className="ri-arrow-left-line text-2xl active:scale-[1.5]"
+              ></i>
+              <h4>Back</h4>
+            </div>
+            <h1 className="text-3xl font-semibold text-center mt-5">
+              Store Your Password
+            </h1>
+            <p className="text-sm font-semibold text-center">
+              Welcome to Password manager to store your password
+            </p>
+          </div>
+          <div className="crtprt-center relative px-4 py-6 flex w-full items-start mt-24 h-[77vh] rounded-t-4xl  bg-white">
+            <form
+              onSubmit={handleOnSubmit}
+              action=""
+              className="flex items-start flex-col w-full mt-12"
+            >
+              <h4 className="absolute top-6 left-[35%] w-full text-xl font-semibold ">
+                Get Started
+              </h4>
+              <input
+                ref={imageRef}
+                required
+                minLength={4}
+                maxLength={16}
+                onChange={handleOnChange}
+                value={userPassword.website}
+                name="website"
+                className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm"
+                placeholder="Enter Your Website ? App Name"
+                type="text"
+              />
+              <input
+                required
+                minLength={4}
+                maxLength={16}
+                onChange={handleOnChange}
+                value={userPassword.username}
+                name="username"
+                className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm"
+                placeholder="Enter Your Username, Email, userid"
+                type="text"
+              />
+              <input
+                required
+                minLength={4}
+                maxLength={16}
+                onChange={handleOnChange}
+                value={userPassword.password}
+                name="password"
+                className="outline-none  border-[1.5px] border-gray-900 mb-3 w-full py-4 px-3 rounded-xl placeholder:text-sm"
+                placeholder="Enter Your Password"
+                type="password"
+              />
+              <button
+                type="submit"
+                className="text-lg bg-[#FFCB47] w-full py-3 rounded-xl mt-2"
+              >
+                {loader ? (
+                  <div className="flex justify-center gap-2 items-center">
+                    <div className="h-6 w-6 animate-spin inline-block border-t-transparent rounded-full border-gray-700 border-[3px]"></div>
+                    Loading
+                  </div>
+                ) : (
+                  "Create"
+                )}
+              </button>
+              <></>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <Signup />
+      )}
+    </>
   );
 };
 
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,80 +1,112 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-const [userdata, setuserdata] = useState({
-    name:"",
-    email:"",
-    password:"",
-})
+  const [userdata, setuserdata] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-const [file, setfile] = useState(null)
-const navigate = useNavigate()
+  const [file, setfile] = useState(null);
+  const navigate = useNavigate();
 
-const Onchange = (e) =>[
-    setuserdata({...userdata , [e.target.name]:e.target.value}),
-    console.log(userdata)
-]
+  const Onchange = (e) => [
+    setuserdata({ ...userdata, [e.target.name]: e.target.value }),
+    console.log(userdata),
+  ];
 
-const handleOnSubmit = (e) =>{
+  const handleOnSubmit = (e) => {
     e.preventDefault();
     // console.log(userdata , file.name)
-    let user = JSON.stringify(userdata)
-    localStorage.setItem("userdata" , user)
-    navigate("/")
-}
+    let user = JSON.stringify(userdata);
+    localStorage.setItem("userdata", user);
+    navigate("/");
+  };
 
-
-const handlefilechange = (e) =>{
-const file = e.target.files[0]
-if(file){
-    setfile(file)
-    console.log("selected file" , file.name)
-}
-}
-
-
-
+  const handlefilechange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setfile(file);
+      console.log("selected file", file.name);
+    }
+  };
 
   return (
-    <div className='h-screen w-full bg-blue-500 relative '>
-        <div onClick={()=>{navigate("/")}} className="flex absolute top-4 left-3 items-center justify-center text-white">
-        <i className="ri-arrow-left-wide-line text-3xl" ></i>
+    <div className="h-screen w-full bg-blue-500 relative ">
+      <div
+        onClick={() => {
+          navigate("/");
+        }}
+        className="flex absolute top-4 left-3 items-center justify-center text-white"
+      >
+        <i className="ri-arrow-left-wide-line text-3xl"></i>
         <h4>Back</h4>
-        </div>
-        <img className='h-screen w-full'  alt="" />
-        <div className="sign-up absolute h-[63vh] w-full bg-slate-100  z-[100] bottom-0 rounded-t-[35px]">
-            <h1 className='text-3xl text-blue-500 font-semibold text-center mt-4'>Get Started</h1>
-            <form onSubmit={handleOnSubmit} action="" className='px-4'>
-                <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
-                    <h4 className='text-[13px] mx-1'>Name</h4>
-                    <input   value={userdata.name} onChange={Onchange} name='name' className='w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]' type="text" placeholder='Enter Your Name' />
-                </div>
+      </div>
+      <img className="h-screen w-full" alt="" />
+      <div className="sign-up absolute h-[63vh] w-full bg-slate-100  z-[100] bottom-0 rounded-t-[35px]">
+        <h1 className="text-3xl text-blue-500 font-semibold text-center mt-4">
+          Get Started
+        </h1>
+        <form onSubmit={handleOnSubmit} action="" className="px-4">
+          <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
+            <h4 className="text-[13px] mx-1">Name</h4>
+            <input
+              value={userdata.name}
+              onChange={Onchange}
+              name="name"
+              className="w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]"
+              type="text"
+              placeholder="Enter Your Name"
+            />
+          </div>
 
-                <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
-                    <h4 className='text-[13px] mx-1'>Email</h4>
-                    <input value={userdata.email} onChange={Onchange} name='email' className='w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]' type="email" placeholder='Enter Your Email' />
-                </div>
+          <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
+            <h4 className="text-[13px] mx-1">Email</h4>
+            <input
+              value={userdata.email}
+              onChange={Onchange}
+              name="email"
+              className="w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]"
+              type="email"
+              placeholder="Enter Your Email"
+            />
+          </div>
 
-                <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
-                    <h4 className='text-[13px] mx-1'>Photo</h4>
-                    <input disabled onChange={handlefilechange} className='w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]' type="file" accept="image/*" placeholder='' />
+          <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
+            <h4 className="text-[13px] mx-1">Photo</h4>
+            <input
+              disabled
+              onChange={handlefilechange}
+              className="w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]"
+              type="file"
+              accept="image/*"
+              placeholder=""
+            />
+          </div>
 
-                </div>
+          <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
+            <h4 className="text-[13px] mx-1">Passowrd</h4>
+            <input
+              value={userdata.password}
+              onChange={Onchange}
+              name="password"
+              className="w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]"
+              type="number"
+              placeholder="Enter Your Password"
+            />
+          </div>
 
-
-                <div className="frm flex items-start justify-start flex-col gap-[5px] mb-5">
-                    <h4 className='text-[13px] mx-1'>Passowrd</h4>
-                    <input value={userdata.password} onChange={Onchange} name='password' className='w-full p-2 outline-none border-[1.5px] border-gray-600 rounded-[10px] placeholder:text-[14px]' type="number" placeholder='Enter Your Password' />
-                </div>
-
-                <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded-[10px]'>Sign up</button>
-
-            </form>
-        </div>
-        
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-[10px]"
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
